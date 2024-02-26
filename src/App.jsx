3fb46cdcbe24a78b29a51b6c04501f609918f723
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-02-26 20:56:14 +0900

    Refactor: main color 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { MantineProvider, createTheme } from '@mantine/core';
 import { Global } from '@emotion/react';
+import Portfolio from './components/Portfolio/Portfolio';
 import GlobalStyles from './styles/GlobalStyles';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 
 const theme = createTheme({
   fontFamily: 'Pretendard, sans-serif',
-  primaryColor: 'green',
+  primaryColor: 'cyan',
   colors: {
     dark: ['#fff', '#adadad', '#828282', '#6c6c6c', '#525252', '#404040', '#2c2c2c', '#121212', '#01010A', '#000000'],
   },
@@ -17,7 +18,7 @@ function App() {
     <>
       <MantineProvider theme={theme} defaultColorScheme="dark">
         <Global styles={GlobalStyles} />
-        <Notifications position="bottom-right" />
+        <Portfolio />
       </MantineProvider>
     </>
   );
