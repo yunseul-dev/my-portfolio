3a2e9226df7d926ce15bae968e41330bdb68e371
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-02-26 20:59:51 +0900

    Feat: Portfolio 컴포넌트 구현

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../Navbar/Navbar';
+import About from '../About/About';
+import Skills from '../Skills/Skills';
+import Projects from '../Projects/Projects';
+import Archiving from '../Archiving/Archiving';
+
+const Portfolio = () => {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Archiving />
+    </>
+  );
+};
+
+export default Portfolio;
